c45839cb3489c4b081eb25721429b52521110c17
ansh9045 <mailansh904@gmail.com>
2026-09-03 19:17:10 +0530

    created react project and added tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-400 bg-indigo-800 flex justify-center items-center h-screen">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
