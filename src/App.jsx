0753e267c50aed3c9df473d1d0160388a2d3d11c
ansh9045 <mailansh904@gmail.com>
2026-09-04 10:42:21 +0530

    built clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import Clock from './components/Clock'
+import Search from './components/Search'
+import Favourites from './components/Favourites'
+import Roll from './components/Roll'
+
+import bug from "./assets/bug.jpg"
+import dragon from "./assets/dragon.jpg"
+import electric from "./assets/electric.jpg"
+import fairy from "./assets/fairy.jpg"
+import fighting from "./assets/fighting.jpeg"
+import fire from "./assets/ghost.jpg"
+import flying from "./assets/grass.jpg"
+import ghost from "./assets/ghost.jpg"
+import grass from "./assets/grass.jpg"
+import ground from "./assets/ground.webp"
+import ice from "./assets/ice.jpg"
+import normal from "./assets/normal.jpg"
+import poison from "./assets/poison.jpg"
+import psychic from "./assets/psychic.jpg"
+
 
 const App = () => {
+  const [background, setBackground] = useState(psychic)
   return (
-    <h1 className="text-3xl font-bold underline text-red-400 bg-indigo-800 flex justify-center items-center h-screen">
-      Hello world!
-    </h1>
+    <div className="flex flex-col justify-center gap-5 items-center h-screen w-screen bg-[#0c1125]">
+  
+
+        <Clock/>
+        <Search/>
+        <div className='flex'>
+          <Roll/>
+          <Favourites/>
+        </div>
+        
+    </div>
   )
 }
 
---
+++ src/components/Clock.jsx
@@ -0,0 +1,19 @@
+import {useEffect, useState} from 'react'
+
+const Clock = () => {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setTime(new Date())
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+  console.log(time)
+  return (
+    <div className="text-8xl font-black  p-4 text-white">{time.toLocaleTimeString()}</div>
+  )
+}
+
+export default Clock
